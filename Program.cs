9340661458f02ba9b04814dc7105dc5958eb4fed
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-14 23:39:40 -0300

    Finalização do Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string qualquercoisa;
+        
+            
+    
+            
+            Console.Write("Digite qualquer coisa:");
+            qualquercoisa = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"{qualquercoisa.ToUpper()}") ;
+
+        }
+    }
+}
